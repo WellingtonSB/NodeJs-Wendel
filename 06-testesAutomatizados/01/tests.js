74857763788ce4efd4d8f41b8c20fe66c759ab21
Wellington <wellingtonsouza2504@gmail.com>
2021-08-05 17:44:43 -0300

    06- tests with mocka

---
+++ 06-testesAutomatizados/01/tests.js
@@ -0,0 +1,9 @@
+const asserts = require('asserts')
+
+describe ('Star Wars Test', function(){
+    //async function({}) === async()=>{}
+    it('deve buscar o r2d2 com o formato correto',async()=>{
+        const expected = [{nome: 'R2-D2', peso: '96'}]
+        const nomeBase = `r2-d2`
+    })
+})
